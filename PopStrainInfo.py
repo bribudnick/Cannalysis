c3917b257a6cb2cc1df3bae9a1a613c72549fc66
bribudnick <82925196+bribudnick@users.noreply.github.com>
2021-05-10 18:41:21 -0500

    Add files via upload

--- PopStrainInfo.py
+++ PopStrainInfo.py
@@ -1,6 +1,17 @@
+  # Header describing the data will be placed here.
+  # Strain name: name of the strain of marijuana
+  # Indica/Sativa/Hybrid: Indicative of the way the plant grows; known by the
+  # way the effects are felt. Sativa strains are typically more energetic, focused
+  # and felt in the head, whereas indica strains are relaxing and influence the body greatly.
+  # Dominant Terpene: In the most basic sense, these are what you
+  # taste, smell and feel from the marijuana. Different terpenes invoke different
+  # medicinal benefits and bodily/emotional feelings. While strains will have a
+  # profile of terpenes, I have listed the dominant terpene for each strain.
+  # Grow Time: the grow time for a plant of this strain, in weeks. 
 import numpy as np
 import matplotlib.pyplot as plt
-data = np.genfromtext("weed.txt", delimiter = ",")
+comments=#
+weed = np.genfromtxt("weed.txt", delimiter = ",")
 strain = weed[:,0]
 ISH = weed[:,1]
 terp = weed[:,2]
